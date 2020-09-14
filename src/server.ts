ddf4e306102a800e16c28bed0ca6bcc9fc034883
Paulo Pacheco <paulo95.emp@gmail.com>
2020-09-14 09:29:51 -0300

    Config vscode debugger

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 app.listen(3333, () => console.log('💻 Server running o port 3333')); // eslint-disable-line
