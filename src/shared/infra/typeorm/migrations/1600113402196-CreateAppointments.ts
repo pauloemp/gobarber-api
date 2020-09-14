08b0feba800e54619b50841db100b75cd1cce3fa
Paulo Pacheco <paulo95.emp@gmail.com>
2020-09-14 17:10:06 -0300

    Create appointments migration

---
+++ src/shared/infra/typeorm/migrations/1600113402196-CreateAppointments.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1600113402196
+  implements MigrationInterface {
+  private table = new Table({
+    name: 'appointments',
+    columns: [
+      {
+        name: 'id',
+        type: 'uuid',
+        isPrimary: true,
+        generationStrategy: 'uuid',
+      },
+      {
+        name: 'provider',
+        type: 'varchar',
+        isNullable: false,
+      },
+      {
+        name: 'date',
+        type: 'timestamp with time zone',
+        isNullable: false,
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table);
+  }
+}
